8731f8443e82bb1268efdd8ddb3a1d290f8b651a
derekcodingtemple <derek@codingtemple.com>
2019-10-29 12:34:31 -0500

    finished basic navbar; not functional

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingBasket } from '@fortawesome/free-solid-svg-icons'
 
 export default class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-sm navbar-light bg-light">
-        <Link className="navbar-brand" to="/">Simple Shopping Cart</Link>
+        <NavLink className="navbar-brand" to="/">Simple Shopping Cart</NavLink>
         <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="collapsibleNavId">
           <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/" activeClassName="selected">Home</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/shop">Shop</Link>
+              <NavLink className="nav-link" to="/shop" activeClassName="active">Shop</NavLink>
             </li>
+          </ul>
+          <ul className="navbar-nav m1-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/cart">Cart</Link>
+              <NavLink className="nav-link" to="/cart" activeClassName="active">
+                <FontAwesomeIcon icon={faShoppingBasket} />
+                &nbsp;<span class="badge badge-secondary">0 | $0.00</span></NavLink>
             </li>
           </ul>
         </div>
